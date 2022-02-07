952716a23162dda768bd4e0a0bfe3c9007adc971
Pliszka <karoliniakkjk@gmail.com>
2022-02-07 19:41:38 +0100

    correct shape of card

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ FONT_NAME = "Courier"
 
 root = Tk()
 root.title('Flashy')
-root.config(bg=BACKGROUND_COLOR, padx=40, pady=40)
+root.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-canvas = Canvas(bg=BACKGROUND_COLOR)
+canvas = Canvas(bg=BACKGROUND_COLOR, width=800, height=526)
 card_front = PhotoImage(file='./images/card_front.png')
-canvas.create_image(300, 300, image=card_front)
+canvas.create_image(400, 263, image=card_front)
+canvas.config(highlightthickness=0)
 canvas.grid(row=0, column=0, rowspan=2)
-canvas_title = canvas.create_text(190, 100, text='Title', font=(FONT_NAME, 15, 'italic'))
-canvas_txt = canvas.create_text(190, 150, text='Word', font=(FONT_NAME, 25, 'bold'))
+canvas_title = canvas.create_text(400, 150, text='Title', font=(FONT_NAME, 40, 'italic'))
+canvas_txt = canvas.create_text(400, 263, text='Word', font=(FONT_NAME, 60, 'bold'))
 root.mainloop()
