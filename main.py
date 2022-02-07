94233f9fc54783e3d4b9f380d1b34c8c7906f799
Pliszka <karoliniakkjk@gmail.com>
2022-02-07 21:38:25 +0100

    prompting new french words (buttons)

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from tkinter import *
+import pandas
+import random
+# Buttons func
 
+
+def next_word():
+    canvas.itemconfig(canvas_txt, text=random.choice(words)['French'])
+
+
+# Data
+words = pandas.read_csv('data/french_words.csv')
+words = words.to_dict(orient="records")
+print(words)
+# UX
 BACKGROUND_COLOR = "#B1DDC6"
 FONT_NAME = "Courier"
 
@@ -12,16 +25,15 @@ card_front_img = PhotoImage(file='./images/card_front.png')
 canvas.create_image(400, 263, image=card_front_img)
 canvas.config(highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
-canvas_title = canvas.create_text(400, 150, text='Title', font=(FONT_NAME, 40, 'italic'))
-canvas_txt = canvas.create_text(400, 263, text='Word', font=(FONT_NAME, 60, 'bold'))
+canvas_title = canvas.create_text(400, 130, text='French', font=(FONT_NAME, 40, 'italic'))
+canvas_txt = canvas.create_text(400, 263, text=random.choice(words)['French'], font=(FONT_NAME, 60, 'bold'))
 
 incorrect_img = PhotoImage(file="./images/wrong.png")
-left_button = Button(image=incorrect_img, highlightthickness=0, bg=BACKGROUND_COLOR)
+left_button = Button(image=incorrect_img, highlightthickness=0, bg=BACKGROUND_COLOR, command=next_word)
 left_button.grid(row=1, column=0)
 
 correct_img = PhotoImage(file="./images/right.png")
-left_button = Button(image=correct_img, highlightthickness=0, bg=BACKGROUND_COLOR)
+left_button = Button(image=correct_img, highlightthickness=0, bg=BACKGROUND_COLOR, command=next_word)
 left_button.grid(row=1, column=1)
 
 root.mainloop()
-
